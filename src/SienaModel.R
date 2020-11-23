fe0fa44994e86583693b62731c2c948895f62884
Omkar Ranadive <omkar.r1997@gmail.com>
2020-11-23 03:16:54 -0600

    Updates to SIENA code

--- src/SienaModel.R
+++ src/SienaModel.R
@@ -1,36 +1,197 @@
-install.packages("RSiena")
-#library(xtable)
-library(RSiena)
-library(statnet)
+######################################################################################
+#
+# Initialization part 
+#
+######################################################################################
 
-?RSiena
-?sienaNet
 
 if (!"igraph" %in% installed.packages()) install.packages("igraph")
 if (!"statnet" %in% install.packages()) install.packages("statnet")
 if (!"intergraph" %in% install.packages()) install.packages("intergraph") 
+if (!"RSiena" %in% install.packages()) install.packages("RSiena") 
+if (!"texreg" %in% install.packages()) install.packages("texreg") 
 
+library(RSiena)
 library(statnet)
 library(dplyr)  # For data manipulation 
 library(intergraph)
 
 sessionInfo() # check other attached packages. 
 list.files()
-connectionsFromEdgesPre <- read.csv("/Users/anuradha/Documents/GitHub/msia-490-snap-project/data/edges_pre.csv")
-connectionsFromEdgesPost <- read.csv("/Users/anuradha/Documents/GitHub/msia-490-snap-project/data/edges_post.csv")
 
-head(connectionsFromEdgesPre)
-head(connectionsFromEdgesPost)
 
-connections_pre <- as.matrix(connectionsFromEdgesPre, matrix.type = "edgelist", directed = FALSE)
-connections_post <- as.matrix(connectionsFromEdgesPost, matrix.type = "edgelist", directed = FALSE)
+adj_pre <- as.matrix(read.table("../data/adj_pre.dat"))
+adj_post <- as.matrix(read.table("../data/adj_post.dat"))
 
-connections_pre<- as.network(connections_pre)
-connections_post<- as.network(connections_post)
 
-plot(connections_pre)
-plot(connections_post)
+# Load the features 
+nu <- as.matrix(read.table("../data/nu.dat"))
+eng <- as.matrix(read.table("../data/Engineer.dat"))
+research <- as.matrix(read.table("../data/Research.dat"))
+manager <- as.matrix(read.table("../data/Manager.dat"))
+director <- as.matrix(read.table("../data/Director.dat"))
 
-connections <- sienaNet(array(c(connections_pre, connections_post)))
+
+######################################################################################
+#
+# SIENA Model Creation 
+#
+######################################################################################
+
+
+
+# Convert to siena object 
+combined_net <- array(c(adj_pre, adj_post), dim=c(1505, 1505, 2))
+sNet <- sienaNet(combined_net)
+nuNet <- - sienaNet(nu, type="behavior")
+engNet <- - sienaNet(eng, type="behavior")
+rNet <- - sienaNet(research, type="behavior")
+mNet <- - sienaNet(manager, type="behavior")
+dNet <- - sienaNet(director, type="behavior")
+
+# Create report 
+# Model 1 effect at a time - First nuNet (Whether users belong to northwestern or not)
+mybehdata <- sienaDataCreate(sNet, nuNet)
+
+myeff <- getEffects(mybehdata)
+myeff$include[]
+
+myeff <- includeEffects(myeff, egoX, altX, sameX, interaction1 = "nuNet")
+mymodel <- sienaModelCreate(useStdInits = TRUE, projname = 'sna_proj')
+myeff
+
+ans1 <- siena07(mymodel, data=mybehdata, effects=myeff, batch=FALSE, verbose=FALSE, 
+                useCluster=TRUE, nbrNodes=4, returnDeps = TRUE)
+
+ans1$theta
+stan_err <- ans1$se
+estimates <- abs(ans1$theta)
+param_sig <- estimates/stan_err 
+
+
+odds_ratio = exp(ans1$theta)
+probs = odds_ratio / (1 + odds_ratio)
+# Option B:
+library(texreg) ## if you haven't installed it, install it first using the command on the top of the script
+# texreg(ans1) ## for LaTex
+screenreg(ans1)
+saveRDS(ans1, 'ans1_nu.rds')
+
+
+
+
+# Model 1 effect at a time - Second EngNet
+mybehdata <- sienaDataCreate(sNet, engNet)
+
+myeff <- getEffects(mybehdata)
+myeff$include[]
+
+myeff <- includeEffects(myeff, egoX, altX, sameX, interaction1 = "engNet")
+mymodel <- sienaModelCreate(useStdInits = TRUE, projname = 'sna_proj')
+myeff
+
+ans1 <- siena07(mymodel, data=mybehdata, effects=myeff, batch=FALSE, verbose=FALSE, 
+                useCluster=TRUE, nbrNodes=4, returnDeps = TRUE)
+
+ans1$theta
+stan_err <- ans1$se
+estimates <- abs(ans1$theta)
+param_sig <- estimates/stan_err 
+
+
+odds_ratio = exp(ans1$theta)
+probs = odds_ratio / (1 + odds_ratio)
+# Option B:
+library(texreg) ## if you haven't installed it, install it first using the command on the top of the script
+# texreg(ans1) ## for LaTex
+screenreg(ans1)
+saveRDS(ans1, 'ans1_eng.rds')
+
+
+
+
+
+# Model 1 effect at a time - Second EngNet
+mybehdata <- sienaDataCreate(sNet, mNet)
+
+myeff <- getEffects(mybehdata)
+myeff$include[]
+
+myeff <- includeEffects(myeff, egoX, altX, sameX, interaction1 = "mNet")
+mymodel <- sienaModelCreate(useStdInits = TRUE, projname = 'sna_proj')
+myeff
+
+ans1 <- siena07(mymodel, data=mybehdata, effects=myeff, batch=FALSE, verbose=FALSE, 
+                useCluster=TRUE, nbrNodes=4, returnDeps = TRUE)
+
+ans1$theta
+stan_err <- ans1$se
+estimates <- abs(ans1$theta)
+param_sig <- estimates/stan_err 
+
+
+odds_ratio = exp(ans1$theta)
+probs = odds_ratio / (1 + odds_ratio)
+
+screenreg(ans1)
+saveRDS(ans1, 'ans1_MNet.rds')
+
+
+
+
+
+# Model 1 effect at a time - Researchers 
+mybehdata <- sienaDataCreate(sNet, rNet)
+
+myeff <- getEffects(mybehdata)
+myeff$include[]
+
+myeff <- includeEffects(myeff, egoX, altX, sameX, interaction1 = "rNet")
+mymodel <- sienaModelCreate(useStdInits = TRUE, projname = 'sna_proj')
+myeff
+
+ans1 <- siena07(mymodel, data=mybehdata, effects=myeff, batch=FALSE, verbose=FALSE, 
+                useCluster=TRUE, nbrNodes=4, returnDeps = TRUE)
+
+ans1$theta
+stan_err <- ans1$se
+estimates <- abs(ans1$theta)
+param_sig <- estimates/stan_err 
+
+
+odds_ratio = exp(ans1$theta)
+probs = odds_ratio / (1 + odds_ratio)
+
+screenreg(ans1)
+saveRDS(ans1, 'ans1_rNet.rds')
+
+
+
+
+
+# Model 1 effect at a time - Second EngNet
+mybehdata <- sienaDataCreate(sNet, dNet)
+
+myeff <- getEffects(mybehdata)
+myeff$include[]
+
+myeff <- includeEffects(myeff, egoX, altX, sameX, interaction1 = "dNet")
+mymodel <- sienaModelCreate(useStdInits = TRUE, projname = 'sna_proj')
+myeff
+
+ans1 <- siena07(mymodel, data=mybehdata, effects=myeff, batch=FALSE, verbose=FALSE, 
+                useCluster=TRUE, nbrNodes=4, returnDeps = TRUE)
+
+ans1$theta
+stan_err <- ans1$se
+estimates <- abs(ans1$theta)
+param_sig <- estimates/stan_err 
+
+
+odds_ratio = exp(ans1$theta)
+probs = odds_ratio / (1 + odds_ratio)
+
+screenreg(ans1)
+saveRDS(ans1, 'ans1_dNet.rds')
 
 
