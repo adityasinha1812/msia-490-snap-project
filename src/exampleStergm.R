31bd15ce1ef2b801f528b4afbe632fa75cbdedda
Amisha1489 <72571456+Amisha1489@users.noreply.github.com>
2020-12-06 15:41:43 -0600

    Stergm model update

--- src/exampleStergm.R
+++ src/exampleStergm.R
@@ -9,6 +9,7 @@ eng <- as.matrix(read.table("../data/Engineer.dat"))
 research <- as.matrix(read.table("../data/Research.dat"))
 manager <- as.matrix(read.table("../data/Manager.dat"))
 director <- as.matrix(read.table("../data/Director.dat"))
+group_node <- as.matrix(read.table("../data/base_node_feature.dat"))
 
 # Setup data
 t1Net <- as.network.matrix(adj_pre, directed = F)
@@ -25,16 +26,20 @@ set.vertex.attribute(t1Net, "manager",manager[,1])
 set.vertex.attribute(t2Net, "manager",manager[,2])
 set.vertex.attribute(t1Net, "director",director[,1])
 set.vertex.attribute(t2Net, "director",director[,2])
+set.vertex.attribute(t1Net, "group_node",group_node[,1])
+set.vertex.attribute(t2Net, "group_node",group_node[,2])
+
 
 netList <- list(t1Net, t2Net)
 # Run model
 # "ergm-terms" in help
 # Add node attribute for the four group members
 model1 <- stergm(netList, 
-                 formation = ~ edges +nodematch("nu", diff=T) +nodematch("eng", diff=T) +nodematch("research", diff=T) +nodematch("manager", diff=T) +nodematch("director", diff=F) +gwesp(decay = 0.25),# +gwdsp(0.1, fixed=T),
+                 formation = ~ edges +nodematch("nu", diff=T) +nodematch("eng", diff=T) +nodematch("research", diff=T) +nodematch("manager", diff=T) +nodematch("director", diff=F) +nodecov("group_node") +gwesp(decay = 0.25),# +gwdsp(0.1, fixed=T),
                  dissolution = ~ edges, 
                  estimate = "CMLE")
 summary(model1)
+par(mar=c(0,0,0,0))
 mcmc.diagnostics(model1)
 d <- gof(model1)
 d
